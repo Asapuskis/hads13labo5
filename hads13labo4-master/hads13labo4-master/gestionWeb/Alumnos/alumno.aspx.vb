46e0ac4baec6e58d62a5bd1df6dbc316c784bd7f
Asapuskis <aguayoasier@gmail.com>
2018-03-23 21:15:03 +0100

    Laboratorio 5 HADS Grupo 13

---
+++ hads13labo4-master/hads13labo4-master/gestionWeb/Alumnos/alumno.aspx.vb
@@ -0,0 +1,12 @@
+﻿Public Class alumnos
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        lblUsuario.Text = Session("usuario")
+    End Sub
+
+    Protected Sub btnCerrarSesion_Click(sender As Object, e As EventArgs) Handles btnCerrarSesion.Click
+        Session.Remove("usuario")
+        Response.Redirect("~/Inicio.aspx")
+    End Sub
+End Class
---
+++ hads13labo4-master/hads13labo4-master/gestionWeb/Inicio.aspx.vb
@@ -0,0 +1,28 @@
+﻿Public Class Inicio
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub btnIniciarSesion_Click(sender As Object, e As EventArgs) Handles btnIniciarSesion.Click
+        Page.Validate()
+        If Page.IsValid Then
+            Dim accesoDatosSQL = New accesoDatosSQL.accesoDatosSQL
+            If accesoDatosSQL.iniciarSesion(txtUsu.Text, txtContr.Text) Then
+                Session("usuario") = txtUsu.Text
+                If (accesoDatosSQL.tipo(txtUsu.Text) = "Profesor") Then
+                    Session("tipoUsuario") = "Profesor"
+                    Response.Redirect("Profesores/profesor.aspx", True)
+                Else
+                    Session("tipoUsuario") = "Alumno"
+                    Response.Redirect("Alumnos/alumno.aspx", True)
+                End If
+                lblError.Visible = False
+            Else
+                lblError.Visible = True
+            End If
+        End If
+
+    End Sub
+End Class
